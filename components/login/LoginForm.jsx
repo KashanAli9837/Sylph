9852d4effa388183cd4adfe0d43bdbbf22656795
Kashan Ali <kashanali.2332@gmail.com>
2025-02-11 19:30:58 -0800

    adjusted sizes

--- components/login/LoginForm.jsx
+++ components/login/LoginForm.jsx
@@ -4,10 +4,10 @@ const LoginForm = () => {
   return (
     <div className="w-full md:w-1/2 h-full rounded-lg flex flex-col justify-center items-center">
       {/* Heading */}
-      <h1 className="font-semibold text-4xl md:text-3xl lg:text-4xl mb-2">
+      <h1 className="font-semibold text-[7.5vw] md:text-3xl lg:text-4xl md:mb-2">
         Welcome to Sylph!
       </h1>
-      <h3 className="text-lg md:text-base lg:text-lg mb-8">
+      <h3 className="text-[3vw] md:text-base lg:text-lg mb-8">
         Let's turn up the volume
       </h3>
 
